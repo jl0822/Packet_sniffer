4767173db0fa9dd7f280ce6aa799aa10f6a059e9
Jeff L <contractor-0011@outlook.com>
2018-10-01 20:23:36 -0400

    Packet Sniffer

---
+++ packet_sniffer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import scapy.all as scapy
+
+def sniff(interface):
+	scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+
+def process_sniffed_packet(packet):
+	if packet.haslayer(http.HTTPRequest):
+		print(packet.show())
+		if packet.haslayer(scapy.Raw):
+			load = packet[scapy.Raw].load
+			keywords = ["username", "user", "login", "password", "pass"]
+			for keyword in keywords:
+				if the keyword in load:
+					print(load)
+					break
+
+
+sniff("eth0")
